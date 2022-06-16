9c4a806f08ce2b0f82046f0b6c12cca6541bd9f4
cinder block games <hello@cinderblockgames.com>
2022-06-16 15:24:00 -0400

    Moved QR codes out into their own page to prevent the default page taking over root-level resources (specifically, the favicons).

--- NanoPingPong.Web/Pages/Index.cshtml.cs
+++ NanoPingPong.Web/Pages/Index.cshtml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using NanoPingPong.Shared.Config;
-using QRCoder;
 
 namespace NanoPingPong.Web.Pages
 {
@@ -19,24 +16,6 @@ namespace NanoPingPong.Web.Pages
         public void OnGet()
         {
             // Nothing additional.
-        }
-
-        public IActionResult OnGetAddress()
-        {
-            return GenerateQRCode(Context.Link);
-        }
-
-        public IActionResult OnGetDonations()
-        {
-            return GenerateQRCode(Context.DonationLink, true);
-        }
-
-        private FileContentResult GenerateQRCode(string value, bool small = false)
-        {
-            var data = QRCodeGenerator.GenerateQrCode(value, QRCodeGenerator.ECCLevel.Q);
-            var code = new PngByteQRCode(data);
-            var qr = code.GetGraphic(small ? 2 : 3);
-            return File(qr, "image/png");
         }
 
     }
